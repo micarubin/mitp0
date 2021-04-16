7351f908217e5d3dc5687d260022bde773e28e77
utnso <mipaur@gmail.com>
2021-04-16 07:26:17 -0300

    pedactio de parte 2 de consigna

--- tp0.c
+++ tp0.c
@@ -2,7 +2,7 @@
  * main.c
  *
  *  Created on: 28 feb. 2019
- *      Author: utnso
+ *      Author: Mica Rubin
  */
 
 #include "tp0.h"
@@ -18,6 +18,8 @@ int main(void)
 	t_log* logger;
 	t_config* config;
 
+
+
 	logger = iniciar_logger();
 
 	//Loggear "soy un log"
@@ -30,28 +32,42 @@ int main(void)
 
 	leer_consola(logger);
 
-
 	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
 
 	//antes de continuar, tenemos que asegurarnos que el servidor esté corriendo porque lo necesitaremos para lo que sigue.
 
 	//crear conexion
-
-	//enviar CLAVE al servirdor
+	//Primero hay que levantar los valores IP y PUERTO del archivo de configuracion con esas claves
+	ip = config_get_string_value(g_config, "IP");
+	puerto = config_get_string_value(g_config, "PUERTO");
+	//creo la conexion
+	conexion = crear_conexion(ip, puerto);
+	//levanto el valor que le corresponde a clave
+	valor = config_get_string_value(g_config, "CLAVE");
+	//le mando el mensaje al servidor
+	enviar_mensaje(valor, conexion);
+	terminar_programa(conexion);
 
 	paquete(conexion);
-
+	//terminar_programa(conexion); ?
 	terminar_programa(conexion, logger, config);
 }
 
-t_log* iniciar_logger(void)
-{
+t_log* iniciar_logger(void){
+	g_logger = log_create("tp0.log", "TP0", 1, LOG_LEVEL_INFO);
+	//logeo msje soy un log
+	log_info(g_logger, "Soy un log");
 
 }
 
-t_config* leer_config(void)
-{
-
+t_config* leer_config(void){
+	char* valor;
+	//abro archivo de config tp0.config
+	g_config = config_create("tp0.config");
+	//levanto valor de clave
+	valor = config_get_string_value(g_config, "CLAVE");
+	//logeo el valor
+	log_info(g_logger, valor);
 }
 
 void leer_consola(t_log* logger)
@@ -60,7 +76,12 @@ void leer_consola(t_log* logger)
 
 	//El primero te lo dejo de yapa
 	leido = readline(">");
-
+	while(*leido != '\0'){
+		log_info(g_logger, leido);
+		free(leido);
+		leido = readline(">");
+	}
+	free(leido);
 
 }
 
